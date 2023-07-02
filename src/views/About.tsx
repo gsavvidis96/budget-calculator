ed56c6aede15249888f9f8adbe20a274d947061f
Giannis Savvidis <giannis.savvidis@slashdata.co>
2023-07-02 19:46:51 +0300

    add about page

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -6,8 +6,33 @@ const About = () => {
       sx={{
         flexGrow: 1,
       }}
+      gap={2}
     >
-      <Typography>About page content pending...</Typography>
+      <Typography variant="h5">About the project</Typography>
+
+      <Typography variant="body1">
+        This project was created by{" "}
+        <Typography
+          variant="body1"
+          component="a"
+          href="http://cv.gsavvidis.com/"
+          target="_blank"
+          sx={{
+            textDecoration: "none",
+            color: "primary.main",
+            "&:hover": {
+              textDecoration: "underline",
+            },
+          }}
+        >
+          Giannis Savvidis
+        </Typography>
+        . It was created for fun and showcasing purposes. The main technologies
+        used include Typescript, React.js, and supabase. Future plans for this
+        project involve migrating the client to Next.js and utilizing the Prisma
+        ORM instead of the supabase JavaScript SDK. This change aims to improve
+        Typescript support and provide more elegant database migrations.
+      </Typography>
     </Stack>
   );
 };
